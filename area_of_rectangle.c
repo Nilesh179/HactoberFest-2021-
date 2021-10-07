4c2c7bf015ff7df39da966aa5d26d271c12d47ce
Nilesh <nileshsuthar179@gmail.com>
2021-10-07 22:06:09 +0530

    Update area_of_rectangle.c

--- area_of_rectangle.c
+++ area_of_rectangle.c
@@ -2,8 +2,10 @@
 
 int main(){
     int l,b,area;
-    printf("Enter the length and breath of Rectangle ");
-    scanf("%d,%d",&l,&b);
+    printf("Enter the length of the Rectangle : ");
+    scanf("%d",&l);
+    printf("Enter the breadth of the Rectangle : ");
+    scanf("%d",&b);
     area=l*b;
     printf("The Area of the Rectangle is %d",area);
     return 0;
